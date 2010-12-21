ee2ad43318e6d6a596240384d2cdc525ac9791fe
Théophile Helleboid - chtitux <chtitux@gmail.com>
2010-12-21 19:24:26 +0100

    Ajout de MTKey, stockage de Key

---
+++ src/keytool/model/MTKey.java
@@ -0,0 +1,33 @@
+package keytool.model;
+
+import java.security.Key;
+
+public class MTKey {
+	private Key key;
+	private char[] password;
+	
+	public MTKey(Key key, char[] password) {
+		this.key = key;
+		this.setPassword(password);
+	}
+
+
+	public Key getKey() {
+		return this.key;
+	}
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	public void setPassword(char[] password) {
+		this.password = password;
+	}
+
+	public char[] getPassword() {
+		return password;
+	}
+	
+	public String toString() {
+		return "clé "+key.getAlgorithm()+", "+key.getFormat()+", "+key.getClass().getName();
+	}
+}
