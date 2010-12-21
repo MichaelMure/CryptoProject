80539cb198e7fdbdcc2c98d12c0fb3ddd6fc09b5
Théophile Helleboid - chtitux <chtitux@gmail.com>
2010-12-21 18:40:53 +0100

    Suppression fichier inutile

--- src/keytool/view/KeytoolListener.java
+++
@@ -1,9 +0,0 @@
-package keytool.view;
-
-import java.util.EventListener;
-
-public interface KeytoolListener extends EventListener {
-	public void keystoreChanged(KeyStoreChangedEvent event);
-	public void elementSelected(ElementSelectedEvent event);
-	public void selectedTabChanged(TabChangedEvent event);
-}
