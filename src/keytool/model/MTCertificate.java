b0cd5c1066e0d749b464b7ead0786604da0582cc
Théophile Helleboid - chtitux <chtitux@gmail.com>
2010-12-22 16:41:37 +0100

    Ajout details depuis exemple cours

--- src/keytool/model/MTCertificate.java
+++ src/keytool/model/MTCertificate.java
@@ -4,6 +4,8 @@ import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 
+import javax.security.auth.x500.X500Principal;
+
 public class MTCertificate {
 	private Certificate certificate;
 
@@ -31,7 +33,16 @@ public class MTCertificate {
 		sb.append("IssuerDN:"+cert.getIssuerDN().toString());
 		sb.append("\nNotBefore:"+cert.getNotBefore().toString());
 		sb.append("\nNotAfter:"+cert.getNotAfter().toString());
-		
+
+	    // récupération de la description X500 de l'émetteur
+		X500Principal issuer = cert.getIssuerX500Principal();
+		// édition de la description selon la RFC2253 (mode par défaut)
+		sb.append("émetteur\n").append(issuer.getName("RFC2253")).append("\n________________\n\n");
+		// récupération de la description X500 du sujet
+		X500Principal subject = cert.getSubjectX500Principal();
+		// édition de la description selon la RFC1779
+		sb.append("sujet\n").append(subject.getName("RFC1779")).append("\n________________\n\n");
+	
 		return sb.toString();
 	}
 	
