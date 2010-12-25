dd938e409c701e86e467565a72ff88eee04905e9
Michael Muré <mure.michael@gmail.com>
2010-12-25 16:27:22 +0100

    ajout de ModelException, non commité

---
+++ src/keytool/model/ModelException.java
@@ -0,0 +1,14 @@
+package keytool.model;
+
+public class ModelException extends Exception {
+
+	private static final long serialVersionUID = 6988414290050131098L;
+
+	public ModelException() {
+	}
+
+	public ModelException(String message) {
+		super(message);
+	}
+
+}
