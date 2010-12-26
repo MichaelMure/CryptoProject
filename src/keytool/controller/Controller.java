d0a4dea29385a0d9b0aa9a6b5f864ad048916906
Théophile Helleboid - chtitux <chtitux@gmail.com>
2010-12-26 17:08:35 +0100

    Nettoyage des import

--- src/keytool/controller/Controller.java
+++ src/keytool/controller/Controller.java
@@ -4,11 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateEncodingException;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JFileChooser;
