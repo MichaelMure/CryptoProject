b9e767c5d696f3fd7797f346ff27110b296b8a9b
Michael Muré <mure.michael@gmail.com>
2010-12-26 23:15:12 +0100

    ajout du champ alias dans la fenetre d'import

--- src/keytool/view/ImportKeyWindow.java
+++ src/keytool/view/ImportKeyWindow.java
@@ -17,8 +17,11 @@ public class ImportKeyWindow extends JFrame {
 	private JButton BtnCancel;
 	private JLabel LblKeyFile;
 	private JLabel LblCertificateFile;
+	private JLabel LblAlias;
+	private JLabel LblVoid;
 	private JTextField FldKeyFile;
 	private JTextField FldCertificateFile;
+	private JTextField FldAlias;
 	
 	public ImportKeyWindow() {
 		initComponents();
@@ -27,16 +30,24 @@ public class ImportKeyWindow extends JFrame {
 	private void initComponents() {
 		LblKeyFile = new JLabel();
 		LblCertificateFile = new JLabel();
+		LblAlias = new JLabel();
+		LblVoid = new JLabel();
 		FldKeyFile = new JTextField();
 		FldCertificateFile = new JTextField();
+		FldAlias = new JTextField();
 		BtnChooseKey = new JButton();
 		BtnChooseCertificate = new JButton();
 		BtnCancel = new JButton();
 		BtnValidate = new JButton();
 
 		setTitle("Import d'une clé/certificat associé");
-		getContentPane().setLayout(new java.awt.GridLayout(3, 3, 10, 10));
+		getContentPane().setLayout(new java.awt.GridLayout(4, 3, 10, 10));
 
+		LblAlias.setText("Alias");
+		getContentPane().add(LblAlias);
+		getContentPane().add(FldAlias);
+		getContentPane().add(LblVoid);
+		
 		LblKeyFile.setText("Fichier de la clé");
 		getContentPane().add(LblKeyFile);
 		getContentPane().add(FldKeyFile);
@@ -48,7 +59,7 @@ public class ImportKeyWindow extends JFrame {
 		getContentPane().add(FldCertificateFile);
 		BtnChooseCertificate.setText("Parcourir");
 		getContentPane().add(BtnChooseCertificate);
-
+		
 		BtnCancel.setText("Annuler");
 		getContentPane().add(BtnCancel);
 		BtnValidate.setText("Valider");
@@ -58,6 +69,7 @@ public class ImportKeyWindow extends JFrame {
 	}
 	
 	public void resetField() {
+		this.FldAlias.setText("");
 		this.FldKeyFile.setText("");
 		this.FldCertificateFile.setText("");
 	}
@@ -80,6 +92,10 @@ public class ImportKeyWindow extends JFrame {
 	
 	public void addIWWindowListener(WindowListener wdLst) {
     	this.addWindowListener(wdLst);
+	}
+	
+	public String getAliasField() {
+		return this.FldAlias.getText();
 	}
 	
 	public String getKeyFileField() {
