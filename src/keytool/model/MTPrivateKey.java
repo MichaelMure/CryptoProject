76e36c70ed07a2f0fbf95564c6a66dda568a7189
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-01-10 22:44:25 +0100

    Suppression import Date inutile

--- src/keytool/model/MTPrivateKey.java
+++ src/keytool/model/MTPrivateKey.java
@@ -28,7 +28,6 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.security.spec.RSAPrivateCrtKeySpec;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 import javax.security.auth.x500.X500Principal;
