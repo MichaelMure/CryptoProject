37c35957b29c8eaafc6534239baca1a764ce96dd
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-01-10 22:42:07 +0100

    Ajout SecretKey

---
+++ src/keytool/model/MTSecretKey.java
@@ -0,0 +1,20 @@
+package keytool.model;
+
+import java.security.Key;
+
+public class MTSecretKey extends MTKey {
+
+	public MTSecretKey(Key key2) {
+		this.key = key2;
+	}
+	
+	public String getDetails() {
+		StringBuffer details = new StringBuffer();
+		details.append(" - Clé publique ----- ");
+		details.append("\nAlgorithme:\n  ").append(this.key.getAlgorithm());
+		details.append("\nFormat :\n  ").append(this.key.getFormat());
+		details.append("\nInstance de :\n  ").append(this.key.getClass().getName());
+		return details.toString();
+	}
+
+}
